0313e2a4354f815a94594c38325d36ee06d235cd
tomiwadoesux <141045581+tomiwadoesux@users.noreply.github.com>
2025-12-15 22:03:59 -0600

    upd

--- app/components/MusicWidget.js
+++ app/components/MusicWidget.js
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Image from "next/image";
 
 const ErrorIcon = ({ className }) => (
